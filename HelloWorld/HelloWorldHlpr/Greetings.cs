bcf8a91dbc48ca02ad69b5d304691429583f1f80
kishore <justinkishore@gmail.com>
2016-10-20 16:12:41 -0500

    created lib

---
+++ HelloWorld/HelloWorldHlpr/Greetings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldHlpr
+{
+    public class Greetings
+    {
+        public string msgInEnglish()
+        {
+            return "Hello World!";
+        }
+    }
+}
